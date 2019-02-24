bef7d1abb487d4321e884af8bf66f4e6a3dd9f20
taylorz <taylor.zanke@gmail.com>
2019-02-24 16:46:52 -0500

    nav

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $('document').ready(function(){
   AOS.init({
     once: true
   });
+
   $("nav").find("a").click(function(e) {
       e.preventDefault();
       var section = $(this).attr("href");
@@ -9,4 +10,15 @@ $('document').ready(function(){
           scrollTop: $(section).offset().top
       });
   });
+
+  var prev = 0;
+  var $window = $(window);
+  var nav = $("nav");
+
+($window).scroll(function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
+
 });
