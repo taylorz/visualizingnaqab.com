c19fb42ad82e8988dd510adfe56480a83c575cf9
taylorz <taylor.zanke@gmail.com>
2019-02-24 16:15:11 -0500

    adding changes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@ $('document').ready(function(){
   AOS.init({
     once: true
   });
+  $("nav").find("a").click(function(e) {
+      e.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+          scrollTop: $(section).offset().top
+      });
+  });
 });
